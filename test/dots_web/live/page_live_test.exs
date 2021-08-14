619fafcd3f5faaee9ccc09273c5a9cecffe3d9fb
AJ Foster <web@aj-foster.com>
2021-08-14 00:23:47 -0400

    Make example tests work

--- test/dots_web/live/page_live_test.exs
+++ test/dots_web/live/page_live_test.exs
@@ -5,7 +5,7 @@ defmodule DotsWeb.PageLiveTest do
 
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+    assert disconnected_html =~ "Create 10"
+    assert render(page_live) =~ "Create 10"
   end
 end

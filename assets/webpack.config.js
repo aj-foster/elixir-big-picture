6ff00b702313ca7302a2dc718cc9bb50b7f81b82
AJ Foster <web@aj-foster.com>
2021-08-14 00:31:03 -0400

    Upgrade JS dependencies

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,29 +1,21 @@
 const path = require('path');
 const glob = require('glob');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => {
   const devMode = options.mode !== 'production';
 
   return {
-    optimization: {
-      minimizer: [
-        new TerserPlugin({ cache: true, parallel: true, sourceMap: devMode }),
-        new OptimizeCSSAssetsPlugin({})
-      ]
-    },
     entry: {
       'app': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
     },
     output: {
-      filename: '[name].js',
       path: path.resolve(__dirname, '../priv/static/js'),
+      filename: '[name].js',
       publicPath: '/js/'
     },
-    devtool: devMode ? 'source-map' : undefined,
     module: {
       rules: [
         {
@@ -40,12 +32,27 @@ module.exports = (env, options) => {
             'css-loader',
             'sass-loader',
           ],
+        },
+        {
+          test: /\.module\.css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
         }
       ]
     },
     plugins: [
       new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-      new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
-    ]
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'static/', to: '../' }
+        ]
+      })
+    ],
+    optimization: {
+      minimizer: [
+        '...',
+        new CssMinimizerPlugin()
+      ]
+    },
+    devtool: devMode ? 'source-map' : undefined
   }
 };
